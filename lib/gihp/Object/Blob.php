3e3b77aa52b36039b4af6d02d9b2594eedebb303
Lars Vierbergen <vierbergenlars@gmail.com>
2012-11-19 19:58:37 +0100

    A blob just creates itself on data import

--- lib/gihp/Object/Blob.php
+++ lib/gihp/Object/Blob.php
@@ -6,4 +6,8 @@ class Blob extends Internal {
     function __construct($data) {
         parent::__construct(parent::BLOB, $data);
     }
+
+    static function import($data) {
+        return new self($data);
+    }
 }
