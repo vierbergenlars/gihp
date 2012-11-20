10c26313c75b6013409fd5ea894482cbe3b8df0e
Lars Vierbergen <vierbergenlars@gmail.com>
2012-11-20 21:57:08 +0100

    Add PHPDocs to gihp\Object

--- lib/gihp/Object/Internal.php
+++ lib/gihp/Object/Internal.php
@@ -4,32 +4,82 @@ namespace gihp\Object;
 
 class Internal
 {
+    /**
+     * Type of the object
+     * @var int
+     */
     private $type;
+    /**
+     * Data in the object
+     * @var string
+     */
     private $data;
+    /**
+     * Object is a commit
+     */
     const COMMIT=1;
+    /**
+     * Object is a blob
+     */
     const BLOB=2;
+    /**
+     * Object is a tree
+     */
     const TREE=3;
+
+    /**
+     * Creates a new Internal object
+     * @param int         $type The type of the object
+     * @param string|null $data The data in the object
+     */
     public function __construct($type, $data=null)
     {
         $this->type = $type;
         $this->data = $data;
     }
 
+    /**
+     * Overwrites the data in the object
+     * @param string $data
+     */
     protected function setData($data)
     {
         $this->data = $data;
     }
 
+    /**
+     * Appends data. Does not overwrite it.
+     * @param string $data
+     */
     protected function appendData($data)
     {
         $this->data.=$data;
     }
 
+    /**
+     * Gets the data stored in here
+     *
+     * Note: only the data, not the checksums and so around it.
+     * @return string
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Gets the SHA1 hash of the object
+     * @return sting
+     */
     public function getSHA1()
     {
         return sha1($this->__toString());
     }
 
+    /**
+     * Gets the type as a string
+     * @return string
+     */
     protected function getTypeString()
     {
         switch ($this->type) {
@@ -44,6 +94,12 @@ class Internal
         }
     }
 
+    /**
+     * The object as it should be written to disk, with all padding
+     *
+     * @internal ALWAYS call this function after adding data with setData() or appendData()
+     * @return string
+     */
     public function __toString()
     {
         $header = $this->getTypeString().' '.strlen($this->data).chr(0);
