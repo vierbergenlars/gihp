7c09c171a5c7c610fa6f8f9c012bc70a1ef72461
Lars Vierbergen <vierbergenlars@gmail.com>
2012-11-20 22:01:13 +0100

    Add the loader for Object\*

---
+++ lib/gihp/Object/Loader.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace gihp\Object;
+
+use gihp\Defer\Loader as DLoader;
+use gihp\IO\IOInterface;
+
+/**
+ * A loader for objects.
+ * Always invokes load on Internal, but that will load the right object.
+ * @internal
+ */
+class Loader implements DLoader {
+    /**
+     * The IO class
+     * @var IOInterface
+     */
+    private $io;
+
+    /**
+     * Creates a new loader
+     * @param IOInterface $io An IO module
+     */
+    function __construct(IOInterface $io) {
+        $this->io = $io;
+    }
+
+    /**
+     * Loads the object
+     * @param string $sha1 The hash of the object to load
+     * @return Internal
+     */
+    function load($sha1) {
+        return $this->io->readObject($sha1);
+    }
+
+}
