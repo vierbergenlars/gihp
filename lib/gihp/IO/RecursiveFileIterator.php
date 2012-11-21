986213802a5c8dfef8bc2bbaeedcb9d2da9699ec
Lars Vierbergen <vierbergenlars@gmail.com>
2012-11-21 20:39:38 +0100

    Add IO\RecursiveFileIterator for use in IO\File

---
+++ lib/gihp/IO/RecursiveFileIterator.php
@@ -0,0 +1,13 @@
+<?php
+namespace gihp\IO;
+
+class RecursiveFileIterator extends \RecursiveDirectoryIterator {
+    function getChildren() {
+        try {
+            return parent::getChildren();
+        }
+        catch(\UnexpectedValueException $e) {
+            return new \RecursiveArrayIterator(array());
+        }
+    }
+}
