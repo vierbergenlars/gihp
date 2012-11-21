49352700fbfbd4e2c2a092bfb070c638e53d8b08
Lars Vierbergen <vierbergenlars@gmail.com>
2012-11-21 01:14:34 +0100

    Add IO\File::moveHead() and ::readHead(). Update IO\IOInterface.
    The symbolic references have nothing to do with normal references,
    So we put them in a separate namespace. Update the interface
    gihp\IO\IOInterface to reflect those changes

--- lib/gihp/IO/File.php
+++ lib/gihp/IO/File.php
@@ -96,12 +96,22 @@ class File implements IOInterface
         return \gihp\Object\Internal::import($loader, $decoded);
     }
 
-    public function moveHead(\gihp\Ref\SymbolicReference $ref)
+    public function moveHead(\gihp\Symref\SymbolicReference $ref)
     {
+        $file = $this->path.'/.git/HEAD';
+        file_put_contents($file, $ref);
     }
 
     public function readHead()
     {
+        $file = $this->path.'/.git/HEAD';
+        if (!is_file($file)) {
+            throw new \RuntimeException('HEAD not found');
+        }
+        $data = file_get_contents($file);
+        $loader = new \gihp\Symref\Loader($this);
+
+        return \gihp\Symref\SymbolicReference::import($loader, $data);
     }
 
     public function gc()
