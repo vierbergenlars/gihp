7d268710296a3173b949c7ee7cde1c64f0193c20
Lars Vierbergen <vierbergenlars@gmail.com>
2012-11-21 15:49:23 +0100

    Add commit function for the branch and remove objects_to_write
    A branch can now contain no commit. It can be written like that,
    but a commit can be added with the commit() function.
    Objects_to_write is obsoleted by IO\WritableInterface on all objects

--- lib/gihp/Branch.php
+++ lib/gihp/Branch.php
@@ -5,7 +5,8 @@ use gihp\IO\IOInterface;
 use gihp\IO\WritableInterface;
 use gihp\Object\Commit;
 use gihp\Ref\Head;
-
+use gihp\Metadata\Person;
+use gihp\Object\Tree;
 /**
  * A git branch
  */
@@ -16,24 +17,26 @@ class Branch implements WritableInterface {
      */
     protected $ref;
     /**
+     * The name of the branch
+     * @var string
+     */
+    protected $name;
+    /**
      * The IOInterface the branch uses
      * @var gihp\IO\IOInterface
      */
     private $io;
-    /**
-     * New added objects that still need to be written
-     * @var array
-     */
-    private $objects_to_write = array();
     /**
      * Creates a new branch
      * @param IOInterface $io The IO to use
      * @param string $name The name of the branch
      * @param Commit $commit The HEAD commit of the branch
      */
-    function __construct(IOInterface $io, $name, Commit $commit) {
+    function __construct(IOInterface $io, $name, Commit $commit=null) {
         $this->io = $io;
-        $this->ref = new Head($name, $commit);
+        $this->name = $name;
+        if($commit)
+            $this->ref = new Head($name, $commit);
     }
 
     /**
@@ -41,7 +44,7 @@ class Branch implements WritableInterface {
      * @return string
      */
     function getName() {
-        return $this->ref->getName();
+        return $this->name;
     }
 
     /**
@@ -49,6 +52,8 @@ class Branch implements WritableInterface {
      * @return Commit
      */
     function getHeadCommit() {
+        if(!$this->ref)
+            return null;
         return $this->ref->getCommit();
     }
 
@@ -57,12 +62,25 @@ class Branch implements WritableInterface {
      * @return array An array of Commit objects
      */
     function getHistory() {
+        if(!$this->getHeadCommit())
+            return array();
         $head = $this->getHeadCommit();
         $commits = array($head);
         while($head = $head->getParent()) {
             $commits[] = $head;
         }
         return $commits;
+    }
+
+    /**
+     * Creates a new commit to the branch
+     * @param string $message The commit message
+     * @param Tree $tree The tree that describes the data in the commit
+     * @param Person $author The author and committer of the commit
+     */
+    function commit($message, Tree $tree, Person $author) {
+        $commit = new Commit($message, $tree, $author, null, $this->getHeadCommit());
+        $this->advanceHead($commit);
     }
 
     /**
@@ -74,12 +92,16 @@ class Branch implements WritableInterface {
     private function advanceHead(Commit $commit) {
         $current = $this->getHeadCommit();
 
+        if(!$current) {
+            $this->ref = new Head($this->name, $commit);
+            return;
+        }
+
         while($parent = $commit->getParent()) {
-            if($parent->getSHA1() == $current->SHA1()) {
+            if($parent->getSHA1() == $current->getSHA1()) {
                 $this->ref->setCommit($commit);
                 return;
             }
-            $this->objects_to_write[] = $parent;
         }
         throw new \RuntimeException('Cannot advance the head to a commit that is not a child of the head commit');
     }
@@ -90,12 +112,15 @@ class Branch implements WritableInterface {
      */
     function write(IOInterface $io=null) {
         if($io === null) $io = $this->io;
-        $io->removeRef($this->ref);
-        $this->ref->write($io);
-        foreach($this->objects_to_write as $commit) {
-            $commit->write($io);
+        if(!$this->ref)
+            throw new \LogicException('Branch cannot be written if no head reference exists');
+        try {
+            $io->removeRef($this->ref);
         }
-
+        catch(\RuntimeException $e) {
+            //Nothing to worry about, the ref just does not exist.
+        }
+        $this->ref->write($io);
     }
 
     /**
