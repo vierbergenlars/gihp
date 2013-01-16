4f028334e58ab48cc55e575d8e03a303f0c63c8c
Lars Vierbergen <vierbergenlars@gmail.com>
2013-01-16 14:36:41 +0100

    Add diff engine and diff merger (Based on @luqui merge3)

---
+++ lib/gihp/Diff/Line/AddLine.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace gihp\Diff\Line;
+
+/**
+ * A line that was added in the diff
+ */
+class AddLine extends Line {
+    protected $prefix = '+';
+}
---
+++ lib/gihp/Diff/Line/Line.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace gihp\Diff\Line;
+
+/**
+ * A standard diff line
+ */
+class Line {
+    const NONE = 0x0;
+    const BASE = 0x1;
+    const HEAD = 0x2;
+    const BOTH = 0x3;
+    private $line;
+    protected $prefix = ' ';
+    /**
+     * Creates a new diff line
+     * @param string $line The contents of the line
+     * @param int $place The place where the line originates from
+     */
+    function __construct($line, $place = self::NONE) {
+        $this->line = $line;
+        $this->place = $place;
+    }
+    
+    /**
+     * Converts the diff line to an internal represenation for the merger
+     * @internal
+     * @return array
+     */
+    function getArray() {
+        return array($this->prefix, $this->line);
+    }
+    
+    /**
+     * Gets the location the line originates from
+     * @return int
+     */
+    function getPlace() {
+        return $this->place;
+    }
+    
+    /**
+     * Gets the contents of the diff line
+     * @return string
+     */
+    function getLine() {
+        return $this->line;
+    }
+    
+    /**
+     * Creates a string representation of the diff line
+     * @return string
+     */
+    function __toString() {
+        return $this->prefix.$this->line;
+    }
+}
