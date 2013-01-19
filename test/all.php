6047585ed90047610f25001f67e07d5d150ea175
Lars Vierbergen <vierbergenlars@gmail.com>
2013-01-19 17:53:19 +0100

    [TEST][IO] Add travis testing for File IO components

---
+++ test/all.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace test;
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/vierbergenlars/simpletest/autorun.php';
+
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->add('test', __DIR__.'/..');
+$loader->register();
+
+class gihpPlumbingTests extends \TestSuite
+{
+    public function __construct()
+    {
+        parent::__construct('Gihp plumbing tests');
+        $this->add(new plumbing\io);
+    }
+}
---
+++ test/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cd "$( dirname "$0" )"
+rm -r repo
+mkdir repo
+cd repo
+git init
+git config user.name gihp
+git config user.email git@gihp
+echo "file1" > file1
+git add file1
+git commit -m "Added file 1"
+git tag v0.0.1
+git branch tests
+git checkout tests
+mkdir test
+echo "file2" > test/file2
+git add test
+git commit -m "Added tests for file 2"
+git tag test-2 -m "Add tests for file2, tba"
+git checkout master
+echo "file2" > file2
+git add file2
+git commit -m "Add file 2"
+git tag v0.0.2
