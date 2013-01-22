7e78e440204cfaf1fc1651520ce09ec7912bfd0e
Lars Vierbergen <vierbergenlars@gmail.com>
2013-01-22 19:58:49 +0100

    Get back packfile support (accepted to be released under MIT)
    Signed-off-by: Lars Vierbergen <vierbergenlars@gmail.com>

--- lib/gihp/IO/File.php
+++ lib/gihp/IO/File.php
@@ -2,6 +2,7 @@
 
 namespace gihp\IO;
 
+use gihp\IO\File\Packfile;
 use gihp\IO\File\Packref;
 use gihp\IO\File\RecursiveFileIterator;
 
@@ -12,6 +13,7 @@ class File implements IOInterface
 {
     private $path;
     private $bare;
+    private $packfile;
     public function __construct($path, $bare = null)
     {
         if ($bare === null && is_dir($path.'/.git')) {
@@ -105,12 +107,10 @@ class File implements IOInterface
 
     public function readObject($sha1)
     {
-        $dir = $this->path.'/objects/'.substr($sha1,0,2);
-        $path = $dir.'/'.substr($sha1,2);
-        if (!is_file($path)) {
-            throw new \RuntimeException('Object not found');
-        }
-        $decoded = gzuncompress(file_get_contents($path));
+        $dir = $this->path.'/objects/';
+        if(!$this->packfile)
+            $this->packfile = new Packfile($dir);
+        $decoded = $this->packfile->getObject($sha1);
         $loader = new \gihp\Object\Loader($this);
 
         return \gihp\Parser\File::importObject($loader, $decoded, $sha1);
@@ -141,6 +141,7 @@ class File implements IOInterface
      */
     public function clearCache()
     {
+        $this->packfile->clearCache();
     }
 
     public function gc()
